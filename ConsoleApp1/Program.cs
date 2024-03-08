352286f4cf1d1a276ac24b23678de02d2a27c222
FoggyDeer <s28254@pjwstk.edu.pl>
2024-03-08 14:59:16 +0100

    GetAvg function added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine(";Hedllo,, World!!");
+Console.WriteLine(getAvg(new int[]{1, 2, 3, 4, 5}));
+static double getAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+
+    return (double)sum / arr.Length;
+}
