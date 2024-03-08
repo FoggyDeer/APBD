fde2a9f46fcce956ed6c467cd3d43b82d9227e84
FoggyDeer <s28254@pjwstk.edu.pl>
2024-03-08 15:09:04 +0100

    Added new function GetMax

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 
 Console.WriteLine(";Hedllo,, World!!");
 Console.WriteLine(getAvg(new int[]{1, 2, 3, 4, 5}));
+Console.WriteLine(GetMax(new int[]{1,2,3,4,5,11,22,55,0}));
 static double getAvg(int[] arr)
 {
     int sum = 0;
@@ -11,4 +12,16 @@ static double getAvg(int[] arr)
     }
 
     return (double)sum / arr.Length;
+}
+
+static int GetMax(int[] arr)
+{
+    int max = 0;
+    foreach (var num in arr)
+    {
+        if (num > max)
+            max = num;
+    }
+
+    return max;
 }
